90ab9720a55dd5fa455ee88587505dffbb993555
Joe <jakatsa5@gmail.com>
2023-12-22 11:08:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.append(newHeader);
+
+const victory = document.getElementById("victory");
+const yourName = "Joe";
+victory.textContent = `${yourName} is the champion`;
